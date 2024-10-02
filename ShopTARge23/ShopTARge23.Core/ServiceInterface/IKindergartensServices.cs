5d29590c555a16b9cf5bebe6742e2471472f35f1
WINDOWS-1DCGN01\Kasutaja <forusetome@gmail.com>
2024-10-02 16:19:37 +0300

    Kindergarten

---
+++ ShopTARge23/ShopTARge23.Core/ServiceInterface/IKindergartensServices.cs
@@ -0,0 +1,18 @@
+﻿using ShopTARge23.Core.Domain;
+using ShopTARge23.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTARge23.Core.ServiceInterface
+{
+    public interface IKindergartensServices
+    {
+        Task<Kindergarten> Create(KindergartenDto dto);
+        Task<Kindergarten> DetailsAsync(Guid id);
+        Task<Kindergarten> Delete(Guid id);
+        Task<Kindergarten> Update(KindergartenDto dto);
+    }
+}
