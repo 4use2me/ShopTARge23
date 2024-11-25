901929bbe168f5637644f3575c9c62050d4eb2fa
WINDOWS-1DCGN01\Kasutaja <forusetome@gmail.com>
2024-11-25 22:37:18 +0200

    DeleteUnusedUsings

--- ShopTARge23/ShopTARge23.Core/Dto/EmailDto.cs
+++ ShopTARge23/ShopTARge23.Core/Dto/EmailDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShopTARge23.Core.Dto
+﻿namespace ShopTARge23.Core.Dto
 {
 	public class EmailDto
 	{
